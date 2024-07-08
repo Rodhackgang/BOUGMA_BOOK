cc73021abf7cbccae01badf93b0bc88ef2f76fb9
Rodhackgang <116571045+Rodhackgang@users.noreply.github.com>
2024-07-08 09:34:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,21 @@ console.log(`Connecting to MongoDB at ${process.env.MONGODB_URI}`);
 mongoose.connect(process.env.MONGODB_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 50000  // Augmenter le délai d'attente à 50 secondes
+  serverSelectionTimeoutMS: 50000,  // Augmenter le délai d'attente à 50 secondes
+  socketTimeoutMS: 60000,           // Délai d'attente pour les sockets
+  connectTimeoutMS: 30000           // Délai d'attente pour la connexion initiale
 })
   .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
-//Bienvenu
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Quitter l'application en cas d'erreur de connexion
+  });
+
+// Accueil de l'API
 app.get('/', (req, res) => {
   res.send('Bienvenue sur notre API');
 });
+
 // Routes
 const bookRoutes = require('./routes/bookRoutes');
 app.use('/ajoutelements', bookRoutes);
